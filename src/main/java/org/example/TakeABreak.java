1a050b3b8a9003dac28b227cb7de6a0ff30e613a
biniyam <biniyy2021@gmail.com>
2024-07-23 21:04:51 -0400

    Java Looping exercise is done

---
+++ src/main/java/org/example/TakeABreak.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.Scanner;
+
+public class TakeABreak {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String answer = "";
+
+        while (!answer.equals("yes")) {
+            System.out.print("Do you want to take a break? ");
+            answer = scanner.nextLine();
+        }
+
+        System.out.println("Taking a break!");
+    }
+}
